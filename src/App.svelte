8de7d21787d8990c8d371b538c3c96714151775d
sin-jun-woo <46078573+sin-jun-woo@users.noreply.github.com>
2025-07-16 16:13:51 +0900

    stelve 실습
    stelve 실습

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<!-- JS 작성 -->
+
+<script>
+  import Main from "./pages/Main.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Write from "./pages/Write.svelte";
+  import Login from "./pages/Login.svelte";
+  import Router from "svelte-spa-router";
+  import Notfound from "./pages/Notfound.svelte";
+  import "./css/style.css";
+
+  const routes = {
+    "/": Main,
+    "/login": Login,
+    "/signup": Signup,
+    "/write": Write,
+    "*": Notfound,
+  };
+</script>
+
+<Router {routes}></Router>
